7b89cb0b013c14f7ca49c6549cc29875a8edd7cb
Jose-Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2015-03-02 01:38:31 +0100

    First sketch of the lib structure

---
+++ examples/libredaq_example_adc.cpp
@@ -0,0 +1,13 @@
+/**********************************************************
+ *  LibreDAQ PC interface library
+ *   (C) 2015 
+ *   License GNU GPL 3
+ **********************************************************/
+
+#include <libredaq.h>
+
+int main(int argc, char **argv)
+{
+	
+	return 0;
+}
